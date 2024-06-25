fc085fdc5623ab0191701ba58a5890e5e5ed181a
josuedavidrh <jrh24555@gmail.com>
2024-06-24 19:27:32 -0500

    v_inicio_pc2

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2024 <a href="https://www.amarok.com" target="_blank">amarok</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
